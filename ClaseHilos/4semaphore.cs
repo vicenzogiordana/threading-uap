6faed949a7840b9d6c6a4db975a2f09928bbf513
amilcarrey <amilcarrey.ar@gmail.com>
2023-06-23 04:17:06 -0300

    Add semaphore example

--- ClaseHilos/4semaphore.cs
+++ ClaseHilos/4semaphore.cs
@@ -8,31 +8,33 @@ namespace ClaseHilos
 {
     internal class _4semaphore
     {
-        static Semaphore semaforo = new Semaphore(2, 2);
+        static SemaphoreSlim semaphore = new SemaphoreSlim(2, 2);
         static void HolaUAP()
         {
-            semaforo.WaitOne();
-            Console.WriteLine($"Proceso {Thread.CurrentThread.Name}");
-            Thread.Sleep(4000);
-            Console.WriteLine("Hola desde " + Thread.CurrentThread.Name);
-            semaforo.Release();
+            Console.WriteLine($"{Thread.CurrentThread.Name} en Fila");
+            semaphore.Wait();
+            Console.WriteLine($"{Thread.CurrentThread.Name} en atencion");
+            Thread.Sleep(3000);
+            Console.WriteLine($"{Thread.CurrentThread.Name} Saliendo");
+            semaphore.Release();
         }
         internal static void Excecute()
         {
             Thread task1 = new Thread(HolaUAP);
             task1.Name = "Hilo 1";
-
             Thread task2 = new Thread(HolaUAP);
             task2.Name = "Hilo 2";
-
-            Thread task3 = new Thread(() => Console.WriteLine("InlineAction"));
+            Thread task3 = new Thread(HolaUAP);
+            task3.Name = "Hilo 3";
+            Thread task4 = new Thread(HolaUAP);
+            task4.Name = "Hilo 4";
 
             //Excecute all tasks
             task1.Start();
             task2.Start();
             task3.Start();
+            task4.Start();
 
-            Console.WriteLine("Hola desde el hilo principal");
             Console.ReadLine();
         }
 
--- ClaseHilos/Program.cs
+++ ClaseHilos/Program.cs
@@ -1,15 +1,16 @@
-﻿//1 - Uso basico de hilos con Threads
+﻿using ClaseHilos;
 
-using ClaseHilos;
-
+//1 - Uso basico de hilos con Threads
 //_1Basico.Excecute();
 
 //2 - Uso de hilos con ThreadPool
-
 //_2ThreadPool.Excecute();
 
 //3- Uso de Barrier
-_3Barrier.Excecute();
+//_3Barrier.Excecute();
+
+//4- Uso de Semaphore
+_4semaphore.Excecute();
 
 
 
